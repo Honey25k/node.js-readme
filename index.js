fc83f31cb884fd167a15c2229fa1dcdb5b3a44fa
honeytech <honey93k@gmail.com>
2022-12-03 17:33:23 -0800

    modified index.js and added license to generateMarkdown.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs'); 
 const inquirer = require('inquirer'); 
 const { generateMarkdown } = require('./utils/generateMarkdown');
+ 
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -38,30 +39,65 @@ const questions = [
     {
         type: "input",
         name: ["installation"],
-        message: "What are the installation intructions for yout project?"
+        message: "What are the installation intructions for your project?"
     },
 
     {
 
         type: "input",
         name: ["test"],
-        message: "What command should be used to run tests?"
-        default: 'npm test'
+        message: "What command should be used to run tests?",
+        default: "npm test" 
     },
 
     {
 
         type: "input",
         name: ["contribution"],
-        message: "What are the contribution guidlines for your project?"
+        message: "What are the contribution guidelines for your project?"
     },
+
+    {
+
+        type: "input",
+        name: ["usage"],
+        message: "What is the usage information for your project"
+    },
+
+    {
+
+        type: "list",
+        name: ["license"],
+        message: "What is the license of your project?", 
+        choices: ["MIT", "GNU LGPL", "ISC", "Unlicense", "none"],
+        default: "MIT" 
+    },
+
 ]; 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err) throw err;
+        console.log('README has been created!')
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions) // Prompt user to get input data
+        .then(input => {
+            return generateMarkdown(input);
+        })
+        .then(markdown => {
+            writeToFile('./dist/README.md', markdown);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
 
 // Function call to initialize app
 init();
